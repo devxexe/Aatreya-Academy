6188e432c5a5fe58ef123dfd02ced750e311ff1e
Divyansh Singh <divyansh87699@gmail.com>
2026-02-11 13:28:37 +0530

    Refactor counter and popup form functionality

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// POP FLOATING CARD STARTS HERE
 const counters = document.querySelectorAll('.counter');
 const speed = 200; // The lower the slower
 
@@ -25,3 +26,54 @@ counters.forEach(counter => {
 
 	updateCount();
 });
+// POP FLOATING CARD ENDS HERE
+
+
+// FLOATING FORM STARTS HERE
+let popupShown = false; 
+
+    window.onscroll = function() {
+        // Scroll calculation
+        let scrollPercent = (window.scrollY / (document.documentElement.scrollHeight - window.innerHeight)) * 100;
+
+        // Ab ye 40% scroll par hi trigger ho jayega (Jaldi aayega)
+        if (scrollPercent > 20 && !popupShown) {
+            document.getElementById('scrollPopup').classList.add('show');
+            popupShown = true; 
+        }
+    };
+
+    function closePopup() {
+        document.getElementById('scrollPopup').classList.remove('show');
+    }
+
+	// FORM SUBMITION START
+	var form = document.getElementById("popup-contact-form");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("submit-btn"); // Button ka status change karne ke liye
+  var data = new FormData(event.target);
+  
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      alert("Thanks! We have received your query.");
+      form.reset();
+      closePopup(); // Form submit hone ke baad popup band ho jaye
+    } else {
+      alert("Oops! There was a problem submitting your form");
+    }
+  }).catch(error => {
+    alert("Oops! Connection error.");
+  });
+}
+form.addEventListener("submit", handleSubmit)
+	// FORM SUBMITION END
+	
+// FLOATING FORM ENDS HERE
